2caa08c5e5364e8e832d641c2a7fbe1e643479c3
Rapeepat Sangsin <igootle@gmail.com>
2025-07-25 16:45:07 +0700

    add countdown component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,8 @@ import {
 // import sampleData from "@/db/sample-data";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
+import DealCountdown from "@/components/deal-countdown";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -18,6 +20,8 @@ const Homepage = async () => {
       )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
     </>
   );
 };
